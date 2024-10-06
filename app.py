e7ddf513ac6abe3a3cc9a8a207db13914d039478
AndresTrujo <202360022@ucc.mx>
2024-10-06 00:26:52 -0600

    commit

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import django
-import requests
-import requests_cache
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,21 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def api_request():
-    session = requests_cache.CachedSession('fruit_cache')
-
-    url = "https://api.jwstapi.com/observation/jw02731002001_02107_00004_mirimage_o002?page=1&perPage=10"
-
-    payload={}
-    headers = {
-        'X-API-KEY': '1a56a3a7-d082-4dec-8aa6-7ceeda672c5c'
-    }
-
-    response = session.request("GET", url, headers=headers, data=payload)
-    results = response.json()
-
-    print(results)
-
 if __name__ == '__main__':
     app.run()
-    api_request()
